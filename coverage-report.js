3c03c5cf3a630540bebf3c74d947bca448e16aef
Wes Johnson <wjohnson@wealthsimple.com>
2019-04-29 08:19:03 -0400

    unused

--- coverage-report.js
+++
@@ -1,8 +0,0 @@
-const { createReporter} = require('istanbul-api')
-const coverageRaw = require('./coverage.json')
-
-const reporter = createReporter()
-
-reporter.add('html')
-
-reporter.write(coverageRaw)
